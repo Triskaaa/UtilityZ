845832aef758a7b7206a756682145e5179943f09
Triskaaa <96921976+Triskaaa@users.noreply.github.com>
2022-01-01 18:02:56 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,9 +3,7 @@ title UtilityZ - Setup
 echo VOUS POUVEZ ENLEVER CE MESSAGE SI VOUS LA SHOUAITEZ
 echo Vous devez avoir installé NodeJS v12 MINIMUM.
 echo Fonctionnes avec toutes les versions plus récentes que la 12
-echo    
 echo ---
-echo    
 echo Prêt à lancer le bot ?
 pause
 npm i aoi.js@4.5.0
