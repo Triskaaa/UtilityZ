472a65ab56a25c88009eaf4c01b89f0d60e695e5
Triskaaa <96921976+Triskaaa@users.noreply.github.com>
2022-01-01 18:05:57 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ echo VOUS POUVEZ ENLEVER CE MESSAGE SI VOUS LA SHOUAITEZ
 echo Vous devez avoir installé NodeJS v12 MINIMUM.
 echo Fonctionnes avec toutes les versions plus récentes que la 12
 echo ---
-echo Pret à lancer le bot ?
+echo Pret a lancer le bot ?
 pause
 npm i aoi.js@4.5.0
 node index.js
