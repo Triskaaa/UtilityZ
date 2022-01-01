1582ffb24baeb56847b46047cdae9771610011bb
Triskaaa <96921976+Triskaaa@users.noreply.github.com>
2022-01-01 18:01:54 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo OFF
+title UtilityZ - Setup
 echo VOUS POUVEZ ENLEVER CE MESSAGE SI VOUS LA SHOUAITEZ
 echo Vous devez avoir installé NodeJS v12 MINIMUM.
 echo Fonctionnes avec toutes les versions plus récentes que la 12
