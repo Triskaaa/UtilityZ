88ff71a1fa58f7037fa9f21dc43c6c819ff161d7
Triskaaa <96921976+Triskaaa@users.noreply.github.com>
2022-01-01 17:58:35 +0100

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo OFF
+echo VOUS POUVEZ ENLEVER CE MESSAGE SI VOUS LA SHOUAITEZ
+echo Vous devez avoir installé NodeJS v12 MINIMUM.
+echo Fonctionnes avec toutes les versions plus récentes que la 12
+echo    
+echo ---
+echo    
+echo Prêt à lancer le bot ?
+pause
+npm i aoi.js@4.5.0
+cls
+node index.js
+title UtilityZ
